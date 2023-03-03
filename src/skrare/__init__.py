682c81fc40c1cfef4b3ae980f48de10f64975890
Harsh Bandhey <raptor419heavy@gmail.com>
2023-03-02 16:31:45 -0800

    Code for direct import

--- src/skrare/__init__.py
+++ src/skrare/__init__.py
@@ -4,6 +4,7 @@ scikit-rare.
 A Scikit Learn compatible implementation of RARE Algorithm.
 """
 
+from .rare import RARE
 __version__ = "0.9.2"
 __author__ = 'Harsh Bandhey'
 __credits__ = 'Cedars-Sinai Health Systems'
