d09b56f5b2ffa4b1dc033b3aeabe9608c526b7c0
Harsh Bandhey <raptor419heavy@gmail.com>
2023-03-02 16:16:55 -0800

    Final Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scikit-rare",
-    version="0.9.1",
+    version="0.9.2",
     author="Harsh Bandhey",
     author_email="harsh.bandhey@cshs.org",
     description="A Scikit Learn compatible implementation of RARE Algorithm",
